ba835045b35cced58683bbcb3cd55ee83971082a
helloworld3200 <92115307+helloworld3200@users.noreply.github.com>
2024-04-30 12:46:49 +0000

    Add lower range selector

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 import './style.css'
 
+function createLowerRangeSelector()
+{
+    let selector = document.getElementById("lowerRangeSelect");
 
+    for (let i = 1; i <= 60; i++)
+    {
+        let option = document.createElement("option");
+
+        option.value = i;
+        option.text = i;
+
+        selector.appendChild(option);
+    }
+}

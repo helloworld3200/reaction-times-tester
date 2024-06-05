d7b6b59d5b89b116749a19eb666c6ea4f4c9a6d8
helloworld3200 <92115307+helloworld3200@users.noreply.github.com>
2024-06-05 22:30:45 +0100

    Add button animations

--- main.js
+++ main.js
@@ -4,6 +4,33 @@ import './css/tester.css'
 
 console.log("Main JS Running");
 
+class TesterBox
+{
+    constructor(id = "tester")
+    {
+        this.box = document.getElementById(id);
+
+        this.createResetButton();
+        this.resetBox();
+    }
+
+    resetBox()
+    {
+        this.box.textContent = "";
+        this.box.appendChild(this.button);
+    }
+
+    createResetButton()
+    {
+        this.button = document.createElement("button");
+        let text = document.createTextNode("Reset");
+        this.button.appendChild(text);
+        this.button.textContent = "Start Test";
+        this.button.className = "startTestButton";
+        addButtonClickAnimation(this.button);
+    }
+}
+
 function createRangeSelector(id, rangeLimit = 60)
 {
     let selector = document.getElementById(id);
@@ -19,10 +46,24 @@ function createRangeSelector(id, rangeLimit = 60)
     }
 }
 
+function addButtonClickAnimation(button, duration = 200)
+{
+    button.addEventListener("click", function()
+    {
+        button.classList.add("clicked");
+        setTimeout(function()
+        {
+            button.classList.remove("clicked");
+        }, duration);
+    });
+}
+
 function main()
 {
     createRangeSelector("lowerRangeSelect");
     createRangeSelector("upperRangeSelect");
+
+    let testerBox = new TesterBox();
 }
 
 main();
